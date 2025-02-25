db991362b852f9941a6a8bf244856dd03f1d96ea
Dominia <oteniajulie@gmail.com>
2025-02-25 17:56:23 +0100

    update footer & add dotenv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import Footer from "@/components/footer";
 import Nav from "@/components/nav";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "JuPlash",
@@ -27,8 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`mulish-font antialiased`}
       >
         <Nav />
         {children}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,58 +1,48 @@
+"use client";
 import { NextPage } from 'next'
 import Image from "next/image";
+import Link from 'next/link'
 
-interface Props {}
+interface Props { }
 
-const Footer: NextPage<Props> = ({}) => {
-  return <div>
-     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                  className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                  href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Image
-                    aria-hidden
-                    src="/file.svg"
-                    alt="File icon"
-                    width={16}
-                    height={16}
-                  />
-                  Learn
-                </a>
-                <a
-                  className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Image
-                    aria-hidden
-                    src="/window.svg"
-                    alt="Window icon"
-                    width={16}
-                    height={16}
-                  />
-                  Examples
-                </a>
-                <a
-                  className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                  href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Image
-                    aria-hidden
-                    src="/globe.svg"
-                    alt="Globe icon"
-                    width={16}
-                    height={16}
-                  />
-                  Go to nextjs.org →
-                </a>
-        </footer>
-  </div>
+const Footer: NextPage<Props> = ({ }) => {
+  return <footer>
+    <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <p>Website built with Next.js and Tailwind.css</p>
+    </div>
+    <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <Link
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href={`${process.env.NEXT_APP_GITHUB_REPO}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/icons8-github.svg"
+          alt="Github icon"
+          width={24}
+          height={24}
+        />
+        Github
+      </Link>
+      <Link
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href={`${process.env.NEXT_APP_LINKEDIN_PROFILE}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/icons8-linkedin.svg"
+          alt="Linkedin icon"
+          width={24}
+          height={24}
+        />
+        LinkedIn
+      </Link>
+    </div>
+  </footer>
 }
 
 export default Footer
