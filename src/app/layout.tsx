6c8b24cd47445f0f8e41c7b60fcec1afc5d941d6
Dominia <oteniajulie@gmail.com>
2025-02-25 17:08:16 +0100

    app init

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Image from "next/image";
+import Footer from "@/components/footer";
+import Nav from "@/components/nav";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "JuPlash",
+  description: "A simple unsplash images display app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Nav />
+        {children}
+        
+       <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+//import Image from "next/image";
+
+export default function Home() {
+  return ( 
+      <main className="flex flex-wrap items-center justify-center">
+        Hello JuPlash
+      </main>
+      
+  );
+}
